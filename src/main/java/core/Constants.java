9711abffc8459f036cd2eeda9a3bc084a2c70059
Johnny Dutra <johnny.dutra@gmail.com>
2023-11-07 17:46:03 -0300

    adds constants file

---
+++ src/main/java/core/Constants.java
@@ -0,0 +1,7 @@
+package core;
+
+public class Constants {
+
+    public static final String DRIVERS_PATH = "/src/main/java/drivers";
+    public static final String WEBDRIVERUNI_BASE_URL = "https://www.webdriveruniversity.com";
+}
